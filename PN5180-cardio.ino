3b99e7af8e782c301d44ad96005a7d7f4db466c1
CrazyRedMachine <crazyredmachine86@gmail.com>
2020-09-20 11:57:14 +0200

    SL015M test

--- PN5180-cardio.ino
+++ PN5180-cardio.ino
@@ -1,54 +1,6 @@
-// NAME: PN5180-CardIO.ino
-//
-// DESC: Implementation of zyp's famous "cardio" using an Arduino and a PN5180-NFC Module
-//       from NXP Semiconductors.
-//
-// Based on PN5180-Library Copyright (c) 2018 by Andreas Trappmann. All rights reserved.
-// Copyright (c) 2020 by CrazyRedMachine.
-//
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public 
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
-//
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-// Lesser General Public License for more details.
-//
-// BEWARE: SPI with an Arduino to a PN5180 module has to be at a level of 3.3V
-// use of logic-level converters from 5V->3.3V is absolutly neccessary
-// on most Arduinos for all input pins of PN5180!
-// If used with an ESP-32, there is no need for a logic-level converter, since
-// it operates on 3.3V already.
-//
-// ADDITIONAL NOTES BY CrazyRedMachine :
-// I tried to plug directly one of my PN5180 module to an Arduino Leonardo and to
-// an Arduino Mega without any issue. That being said, Arduino Due is probably a 
-// better choice since it supports this code and operates on 3.3V.
-//
-// Arduino <-> Level Converter <-> PN5180 pin mapping:
-// 5V             <-->             5V
-// 3.3V           <-->             3.3V
-// GND            <-->             GND
-// 5V      <-> HV
-// GND     <-> GND (HV)
-//             LV              <-> 3.3V
-//             GND (LV)        <-> GND
-// SCLK,13 <-> HV1 - LV1       --> SCLK
-// MISO,12        <---         <-- MISO
-// MOSI,11 <-> HV3 - LV3       --> MOSI
-// SS,10   <-> HV4 - LV4       --> NSS (=Not SS -> active LOW)
-// BUSY,9         <---             BUSY
-// Reset,7 <-> HV2 - LV2       --> RST
-//
-
 //Set to 0 to disable keypad matrix
 #define WITH_KEYPAD 1
 
-#include "src/PN5180/PN5180.h"
-#include "src/PN5180/PN5180FeliCa.h"
-#include "src/PN5180/PN5180ISO15693.h"
 #include "src/Cardio.h"
 
 #if WITH_KEYPAD == 1
@@ -56,32 +8,35 @@
   #include <Keyboard.h>
 #endif
 
-#define PN5180_NSS  10
-#define PN5180_BUSY 9
-#define PN5180_RST  7
-
-PN5180FeliCa nfcFeliCa(PN5180_NSS, PN5180_BUSY, PN5180_RST);
-PN5180ISO15693 nfc15693(PN5180_NSS, PN5180_BUSY, PN5180_RST);
-
 Cardio_ Cardio;
 
 #if WITH_KEYPAD == 1
-  /* Keypad declarations */
-  const byte ROWS = 4;
-  const byte COLS = 3;
-  /* This is to use the toprow keys */
-  char numpad[ROWS][COLS] = {
-   {'7', '8', '9'},
-   {'4', '5', '6'},
-   {'1', '2', '3'},
-   {'0', ',', '\337'}
-  };
+/* Keypad declarations */
+const byte ROWS = 4;
+const byte COLS = 3;
+/* This is to use the toprow keys */
+char numpad[ROWS][COLS] = {
+  {'7', '8', '9'},
+  {'4', '5', '6'},
+  {'1', '2', '3'},
+  {'0', ',', '\337'}
+};
 
-  byte rowPins[ROWS] = {1, 6, 5, 3}; //connect to the row pinouts of the keypad
-  byte colPins[COLS] = {2, 0, 4}; //connect to the column pinouts of the keypad
-  Keypad kpd = Keypad( makeKeymap(numpad), rowPins, colPins, ROWS, COLS );
+/* For mini keypad */
+byte rowPins[ROWS] = {4, 9, 8, 6}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {5, 3, 7}; //connect to the column pinouts of the keypad
+Keypad kpd = Keypad( makeKeymap(numpad), rowPins, colPins, ROWS, COLS );
 #endif
- 
+
+/* For NFC */
+//SL015M out on pin 2
+#define R1_DET 2
+//Serial on pins 0(RX), 1(TX)
+#define R1_SER Serial1
+#define RFID_BAUD 115200
+int rfSENSOR;
+HardwareSerial* rfSerial;
+
 void setup() {
 
 #if WITH_KEYPAD == 1
@@ -91,28 +46,21 @@ void setup() {
 #endif
 
 /* NFC */
-  nfcFeliCa.begin();
-  nfcFeliCa.reset();
+rfSENSOR = R1_DET;
+rfSerial = &R1_SER;
 
-  uint8_t productVersion[2];
-  nfcFeliCa.readEEprom(PRODUCT_VERSION, productVersion, sizeof(productVersion));
-  if (0xff == productVersion[1]) { // if product version 255, the initialization failed
-    exit(-1); // halt
-  }
-  
-  uint8_t firmwareVersion[2];
-  nfcFeliCa.readEEprom(FIRMWARE_VERSION, firmwareVersion, sizeof(firmwareVersion));
-  uint8_t eepromVersion[2];
-  nfcFeliCa.readEEprom(EEPROM_VERSION, eepromVersion, sizeof(eepromVersion));
-  nfcFeliCa.setupRF();
+pinMode(rfSENSOR,INPUT);                            // Rfid sensor
+rfSerial->begin(RFID_BAUD);
 
+/* HID */
   Cardio.begin(false);
 }
 
 uint32_t loopCnt = 0;
 unsigned long lastReport = 0;
+unsigned long timesent = 0;
 int cardBusy = 0;
-
+bool rfcmdsent = false;
 // read cards loop
 void loop() {
 #if WITH_KEYPAD == 1
@@ -120,47 +68,90 @@ void loop() {
   keypadCheck();
 #endif
   
-  /* NFC */
+  /* NFC (check for ISO-15693 card) */
   if (millis()-lastReport < cardBusy) return;
   
   cardBusy = 0;
   uint8_t uid[8] = {0,0,0,0,0,0,0,0};
-  uint8_t hid_data[8] = {0,0,0,0,0,0,0,0};
 
-  // check for FeliCa card
-  nfcFeliCa.reset();
-#if WITH_KEYPAD == 1
-  keypadCheck();
-#endif
-  nfcFeliCa.setupRF();
-  uint8_t uidLength = nfcFeliCa.readCardSerial(uid);
-    if (uidLength > 0) {
-      Cardio.setUID(2, uid);
-      Cardio.sendState();
-      lastReport = millis();
-      cardBusy = 3000;
-      uidLength = 0;
-      return;
-    }
+  if (digitalRead(rfSENSOR)==LOW)            // card presence
+  {
+          byte rfidp[256];            // rfid response buffer
+      byte rf_i;         //response buffer size
+    if (!rfcmdsent)
+    {
+      rfSerial->write(0xBA);                  // pream
+      rfSerial->write(0x02);                  // size
+      rfSerial->write(0x31);                  // rfidcommand: get tag info
+      rfSerial->write(0xBA ^ 0x02 ^ 0x31);    // checksum
 
-   // check for ISO-15693 card
-  nfc15693.reset();
-#if WITH_KEYPAD == 1
-  keypadCheck();
-#endif
-  nfc15693.setupRF();
-  // try to read ISO15693 inventory
-  ISO15693ErrorCode rc = nfc15693.getInventory(uid);
-  if (rc == ISO15693_EC_OK ) {
-    for (int i=0; i<8; i++) {
-      hid_data[i] = uid[7-i];
+      rf_i = 0;
+      rfcmdsent= true;
+      timesent = millis();
     }
-    Cardio.setUID(1, hid_data);
-    Cardio.sendState();
-      lastReport = millis();
-      cardBusy = 3000;
-    return;
+    else // if command has been sent, let's read answer
+    {
+      if (rfSerial->available()>0)
+      {
+        rfidp[rf_i] = rfSerial->read();
+        rf_i++;
+        if (rfidp[0] != 0xBD)                // restart filling buffer from begining until received byte is 0xBD (preamb)
+          rf_i = 0;
+
+        if (rf_i>2)                          // if we know paquet length
+        {
+          byte length = rfidp[1];
+          if (rf_i >= length + 2)            // if paquet complete
+          {
+            // checksum check
+            byte chk = 0x00;
+            for (int i=0;i< length+1;i++)
+              chk ^= rfidp[i];
+
+            if (chk != rfidp[length+1])
+            {
+              rfcmdsent = false;             // checksum error, send cmd again
+            }
+            else
+            {
+              if (rfidp[3]!=0x00)            // Operation not succeeded
+              {
+                rfcmdsent = false;           // error, send again
+              }
+              else
+              {
+              //success, let's copy the uid
+                for (int i=0;i<8;i++)
+                {
+                  uid[i] = rfidp[11-i];
+                }
+                /*
+                if (uid[0] == 0xE0 && uid[1] == 0x04) // if correct konami card
+                  card = 1;
+                else
+                  card = 0;
+                */
+                Cardio.setUID(1, uid);
+                Cardio.sendState();
+                lastReport = millis();
+                cardBusy = 3000;
+                return;
+              }
+            }
+          }
+        }
+      }
+      else
+      {
+      // command sent, but nothing to receive
+        if (millis() - timesent >500)        // if last command was sent more than 500ms ago, send a new one
+          rfcmdsent = false;
+      }
+    }
   }
+
+  keypadCheck();
+
   // no card detected
   lastReport = millis();
   cardBusy = 200;
@@ -190,6 +181,6 @@ void keypadCheck(){
       }
     }
   }
-  
 }
 #endif
+
--- README.md
+++ README.md
@@ -1,63 +1,33 @@
-# PN5180-cardio (keypad-matrix version)
-PN5180 eAmusement wavepass USB HID card reader (cardio) with integrated keypad
+# ~~PN5180~~ SL015M-cardio (keypad-matrix version)
+~~PN5180~~ SL015M eAmusement wavepass USB HID card reader (cardio) with integrated keypad
 
-# Acknowledgments
-
-This work is based on zyp's cardio (obviously).
-
-ISO15693 code is based on [ATrappmann/PN5180-Library](https://github.com/ATrappmann/PN5180-Library).
-
-FeliCa code is inspired by [tueddy/PN5180-Library/ISO14443](https://github.com/tueddy/PN5180-Library/tree/ISO14443) ISO14443 implementation.
-
-HID layer code is based on Matthew Heironimus' <Joystick.h> library.
-
-The keypad code uses the Keypad library by Mark Stanley and Alexander Brevig.
+This is a quick and dirty adaptation to SL015M as it was used in many ACRealIO devices, to allow owners to use it as cardio as well.
 
 # Supported devices
 
-This code has been tested on Arduino Due, Leonardo, and Pro Micro.
+Leonardo, Pro Micro
 
-Arduino Due is recommended because it is 3.3v logic (or Pro Micro in 3.3v mode).
-According to the PN5180 datasheet, a 5v<->3.3v level shifter is required
-for use an Arduino Leonardo. However, I tested without it and everything
-was working properly, and my PN5180 is not fried. I still recommend a Due
-or a 3.3V pro micro just in case.
+Note that the SL015M doesn't support FeliCa cards.
 
 # Pinout
 
-Because PN5180 is an SPI device, some pins are constrained,
+SL015M out on pin 2
+SL015M Serial interface on pins 0(Arduino RX, ex 19), 1(Arduino TX)
 
-Arduino | Level Converter | PN5180 pin mapping:
---- | --- | ---
-5V      |      <-->       |      5V
-3.3V    |       <-->      |       3.3V
-GND    |        <-->      |       GND
-5V      | HV | .
-GND     | GND (HV) | .
- .        |   LV              | 3.3V
-  .      |    GND (LV)        | GND
-SCLK    | HV1 - LV1       | SCLK
-MISO      |     <---        | MISO
-MOSI    | HV3 - LV3       | MOSI
-10   | HV4 - LV4      | NSS (=Not SS -> active LOW)
-9  |       <---     |        BUSY
-7 | HV2 - LV2      | RST
+If you're coming from an arduino MEGA with your ACRealIO:
 
-Please refer to your Arduino pinout to find SCK, MISO and MOSI pins
-(on Due and Leonardo they are on the SPI / ICSP header, on Pro Micro
-they are pins 15 (SCK),14 (MISO),16 (MOSI))
-
-Unlike the other pins, NSS BUSY and RST can be moved to other gpios
-if you need to.
+- wire that goes to MEGA pin 20 now goes to Leonardo/ProMicro pin 2
+- wire that goes to MEGA pin 19 now goes to Leonardo/ProMicro pin 0
+- wire that goes to MEGA pin 18 now goes to Leonardo/ProMicro pin 1
 
 # Keypad Pinout
 
-Connect the row pins to gpio 1 6 5 3.
-Connect the column pins to gpio 2 0 4.
+Connect the row pins to gpio 4 9 8 6.
+Connect the column pins to gpio 5 3 7.
 
 # How to use
 
-Download zip, install Keypad library by Stanley and Brevig directly from Arduino IDE, flash the firmware, unplug, connect the PN5180 and keypad to the Arduino.
+Download zip, install Keypad library by Stanley and Brevig directly from Arduino IDE, flash the firmware, unplug, connect the SL015M and keypad to the Arduino.
 
 Congratulations, your device should work just like a real cardio, use 
 your favorite tools instructions to play (e.g. "spicetools -cardio" ),
