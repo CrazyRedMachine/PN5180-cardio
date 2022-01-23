4199c123cfb90ea9002d4376423f4dad654636c4
CrazyRedMachine <crazyredmachine86@gmail.com>
2022-01-23 20:02:07 +0100

    id

---
+++ PN5180-cardio/Config.h
@@ -0,0 +1,7 @@
+#ifndef CONFIG_h
+#define CONFIG_h
+
+#define CARDIO_ID 1 //Player ID (1 or 2)
+#define CUSTOM_VIDPID 1 //Enable custom VID/PID (will require to hold reset button to reflash firmware)
+
+#endif
